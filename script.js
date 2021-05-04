c72bf74f67cc3cd7c8e224e5ae3517ccd5762c2f
Matt S <Mpshafransky@Gmail.com>
2021-05-04 18:51:22 -0400

    added more code

---
+++ script.js
@@ -0,0 +1,10 @@
+var searchBtn = document.getElementsByClassName("searchBtn");
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+
+  
